2d64e8d2f71d332d98ebeea0752c3e415550c786
Brandon Brasson <rocksong622@gmail.com>
2019-04-03 20:10:00 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
 class Person
 
     attr_accessor :name, :birthday, :hair_color, :eye_color, :height,:weight, :handed, :complexion, :t_shirt_size,:wrist_size, :glove_size, :pant_length, :pant_width
-     @@all=[]
-    def initialize(attributes)
-    attributes.each do map { |e|  }
 
- end
+    def initialize(attributes)
+      attributes.each do |name, value|
+        self.send(("#{name}="), value)
+      end
     end
   end
