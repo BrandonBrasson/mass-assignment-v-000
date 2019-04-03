edd4462bf3b9924a798914251de81c5bf510dbb2
Brandon Brasson <rocksong622@gmail.com>
2019-04-03 20:05:21 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
     attr_accessor :name, :birthday, :hair_color, :eye_color, :height,:weight, :handed, :complexion, :t_shirt_size,:wrist_size, :glove_size, :pant_length, :pant_width
 
     def initialize(attributes)
-      self.attributes.each do |key, value|
+      all? { |e|  }
 
       end
     end
